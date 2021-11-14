191f4776e0d9e3695c13bff9338df29868f93e20
ghostian <junxiao.guo@outlook.com>
2021-11-15 00:25:39 +0800

    add: more jquery

--- studies/study_webdev/study_jquery/index.js
+++ studies/study_webdev/study_jquery/index.js
@@ -2,9 +2,41 @@ $("h1").addClass("big-title margin-50")
 $("h1").text("GoodBye")
 // $("h1").removeClass("big-title")
 
-// $("button").text("Dont Click Me");
-$("button").html("<em>Bye</em>");
 
 console.log($("img").attr("src"));
 
-$("a").attr("href","https://www.baidu.com")
+$("a").attr("href", "https://www.baidu.com")
+
+// Add event listener
+
+$("h1").click(function () {
+    $("h1").css("color", "blue");
+});
+
+// for (var i = 0; i < 5; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click", function () {
+//         document.querySelector("h1").style.color = "purple";
+//     })
+// }
+
+$("button").click(function () {
+    $("h1").css("color", "purple");
+    // $("button").text("Dont Click Me");
+    $("button").html("<em>Bye</em>");
+})
+
+$("input").keydown(function (event) {
+    console.log(event.key)
+
+})
+
+$(document).keydown(function (event) {
+    console.log(event.key);
+    $("h1").text(event.key);
+
+})
+
+
+$("h1").on("mouseover",function (event) {
+    $("h1").css("color", "green");
+})
