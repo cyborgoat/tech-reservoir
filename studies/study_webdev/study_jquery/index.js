d2d5a50e972c77b71b76e7a16107a69145067317
ghostian <junxiao.guo@outlook.com>
2021-11-15 00:16:26 +0800

    add: jquery manipulations

--- studies/study_webdev/study_jquery/index.js
+++ studies/study_webdev/study_jquery/index.js
@@ -1,1 +1,10 @@
-$("h1").css("color", "red")
+$("h1").addClass("big-title margin-50")
+$("h1").text("GoodBye")
+// $("h1").removeClass("big-title")
+
+// $("button").text("Dont Click Me");
+$("button").html("<em>Bye</em>");
+
+console.log($("img").attr("src"));
+
+$("a").attr("href","https://www.baidu.com")
