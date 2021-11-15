b3682777073414f8e025e155681f2fbcf296722a
ghostian <junxiao.guo@outlook.com>
2021-11-16 00:00:43 +0800

    add:ejs

---
+++ studies/study_webdev/study_ejs/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/",function (req,res){
+    var today = new Date();
+
+    if (today.getDay() === 6 || 0){
+        res.send("Yay it's the weekend!");
+    } else{
+        res.send("Boo! I have to work!");
+    }
+});
+
+
+app.listen(3000,function () {
+    console.log("Server started on port 3000");
+})
+
