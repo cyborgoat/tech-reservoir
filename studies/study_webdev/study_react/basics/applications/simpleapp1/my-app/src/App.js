aefc4990f23659838bc068d527adf8750ad2e79c
ghostian <junxiao.guo@outlook.com>
2021-11-17 23:48:14 +0800

    update: react lecture

--- studies/study_webdev/study_react/basics/applications/simpleapp1/my-app/src/App.js
+++ studies/study_webdev/study_react/basics/applications/simpleapp1/my-app/src/App.js
@@ -1,9 +1,13 @@
+import ExpenseItems from "./components/ExpenseItems";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <p>I know it works</p>
+            <ExpenseItems></ExpenseItems>
+        </div>
+    );
 }
 
 export default App;
---
+++ studies/study_webdev/study_react/basics/applications/simpleapp1/my-app/src/components/ExpenseItems.js
@@ -0,0 +1,6 @@
+function ExpenseItems() {
+    return <h2>Expense Item!</h2>
+
+}
+
+export default ExpenseItems;
