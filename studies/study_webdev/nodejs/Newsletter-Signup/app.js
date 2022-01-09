562285e5fbaded816a5f943e3901eb48b61f5f3b
ghostian <cyborgoat@outlook.com>
2022-01-09 22:37:15 +0800

    add: able to send data to mailchimp

--- studies/study_webdev/nodejs/Newsletter-Signup/app.js
+++ studies/study_webdev/nodejs/Newsletter-Signup/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -19,9 +20,45 @@ app.post("/", function (req, res) {
     var lastName = req.body.lname;
     var email = req.body.email;
     console.log(firstName + lastName + email);
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    const url = "https://us20.api.mailchimp.com/3.0/lists/d77fe71ad4";
+
+    const options = {
+        method: "POST",
+        auth: "cyborgoat1:006cd6757ca1376022d2e3e3db3b6244-us20"
+    }
+
+    const request = https.request(url, options, function (response) {
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
 })
 
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000...");
 });
+
+//API Key
+// 006cd6757ca1376022d2e3e3db3b6244-us20
+//List ID
+// d77fe71ad4
