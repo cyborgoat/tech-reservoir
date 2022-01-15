4d47661b41378dab19c34de81f0fbd8a5ad91b74
ghostian <cyborgoat@outlook.com>
2022-01-15 10:34:15 +0800

    add: projects

--- studies/study_webdev/nodejs/Newsletter-Signup/app.js
+++ studies/study_webdev/nodejs/Newsletter-Signup/app.js
@@ -44,9 +44,18 @@ app.post("/", function (req, res) {
     }
 
     const request = https.request(url, options, function (response) {
-        response.on("data", function (data) {
-            console.log(JSON.parse(data));
-        })
+        if (response.statusCode === 200) {
+            // res.send("Successfully subscribed!");
+            res.sendFile(__dirname + '/success.html')
+        } else {
+            res.sendFile(__dirname + '/failure.html')
+
+            // res.send("There was an error with signing up, please try again!");
+        }
+
+        // response.on("data", function (data) {
+        //     console.log(JSON.parse(data));
+        // })
     });
 
     request.write(jsonData);
