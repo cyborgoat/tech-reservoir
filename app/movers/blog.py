cfb96ccb3c8c1a42bfd0a4c3703a6c6cdc107dff
cyborgoat <cyborgoat@outlook.com>
2023-02-10 00:08:02 +0800

    updated: blog info && dockerfile

--- app/movers/blog.py
+++ app/movers/blog.py
@@ -8,44 +8,46 @@
 @License :   (C)Copyright 2022-2023, Junxiao Guo
 @Desc    :   Sync data and send to destination API endpoint
 '''
-from pathlib import Path
-import os
 from glob import glob
-import frontmatter
-import logging
+from pathlib import Path
 from typing import Dict, List
+
+import os
+import logging
 import requests
+import frontmatter
+import datetime
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
 API_HOST = os.environ.get("API_HOST")
 API_PORT = os.environ.get("API_PORT")
 API_TOKEN = os.environ.get("API_TOKEN")
+BLOG_BASE_DIR = "/home/data/tech-blog"
 
 
 def blog_list() -> List[Dict]:
     """Fetch blog list"""
-    BASE_DIR = "/home/data/tech-blog"
-    category_dirs = glob(f'{BASE_DIR}/*/', recursive=False)
-    blog_list = []
-    required_fields = set(["title", "author", "summary", "date", "tags"])
-    for d in category_dirs:
-        file_list = [i for i in os.listdir(d) if i.endswith('.md')]
-        for f in file_list:
-            fp = Path(d).joinpath(f)
-            fm = frontmatter.load(fp)
+    data = []
 
-            if not fm.metadata or\
-                    set(fm.metadata.keys()) != required_fields:
+    category_dirs = glob(f'{BLOG_BASE_DIR}/*/', recursive=False)
+    required_fields = set(["title", "author", "summary", "date", "tags"])
+
+    for cat_dir in category_dirs:
+        file_list = [i for i in os.listdir(cat_dir) if i.endswith('.md')]
+        for file in file_list:
+            filepath = Path(cat_dir).joinpath(file)
+            front_matter = frontmatter.load(filepath)
+
+            if not front_matter.metadata or\
+                    set(front_matter.metadata.keys()) != required_fields:
                 continue
 
             info = {}
-            info.update(fm.metadata)
-            info.update(content=fp.read_text())
-            blog_list.append(info)
-
-    logging.info("Content fetched.")
-    return blog_list
+            info.update(front_matter.metadata)
+            info.update(content=filepath.read_text(encoding='utf-8'))
+            data.append(info)
+    return data
 
 
 def post_blogs():
@@ -56,12 +58,13 @@ def post_blogs():
 
     data = blog_list()
     for blog in data:
-        blog['date'] = blog['date'].strftime('%Y-%m-%d')
-        blog['created_on'] = blog['date']
-        tags = blog['tags']
-        tags_dict = [{"name": tag} for tag in tags]
-        blog['tags'] = tags_dict
+        blog['created_on'] = blog.pop(
+            'date', datetime.date.today()).strftime('%Y-%m-%d')
+        blog['tags'] = [{"name": tag} for tag in blog.get('tags', [])]
+
         response = requests.post(url, headers=headers,
                                  json=blog,
                                  timeout=300)
-        print("Status Code", response.status_code)
+        if response.status_code != 201:
+            logging.error("%s failed to post",
+                          blog.get("title", "Unknown Blog"))
