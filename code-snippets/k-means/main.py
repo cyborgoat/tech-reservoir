0720764e7f0a7355670e98bb46dc56da145d00a0
cyborgoat <cyborgoat@outlook.com>
2023-05-14 16:41:24 +0800

    added: kmeans

--- code-snippets/k-means/main.py
+++ code-snippets/k-means/main.py
@@ -1,43 +1,41 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import math
+import random
 
-
+# Function to calculate Euclidean distance
 def euclidean_distance(a, b):
-    return np.sqrt(np.sum((a - b) ** 2, axis=1))
+    return math.sqrt(sum([(i - j) ** 2 for i, j in zip(a, b)]))
 
+# Function to calculate mean
+def mean(points):
+    length = len(points)
+    return tuple(sum(p[i] for p in points) / length for i in range(len(points[0])))
 
-def k_means(X, centroids, max_iters=100):
-    k = centroids.shape[0]
+# K-means function
+def k_means(data, k, max_iters=100):
+    # Initialize centroids randomly from the data points
+    centroids = random.sample(data, k)
+
     for _ in range(max_iters):
-        # Step 2: Assign each data point to the nearest centroid
-        labels = np.argmin([euclidean_distance(X, c) for c in centroids], axis=0)
+        # Assign each data point to the nearest centroid
+        labels = [min(range(k), key=lambda i: euclidean_distance(point, centroids[i])) for point in data]
 
-        # Step 3: Update the centroids
-        new_centroids = np.array([X[labels == i].mean(axis=0) for i in range(k)])
+        # Calculate new centroids as the mean of the assigned points
+        new_centroids = [mean([point for point, label in zip(data, labels) if label == i]) for i in range(k)]
 
         # Check for convergence
-        if np.all(centroids == new_centroids):
+        if centroids == new_centroids:
             break
 
         centroids = new_centroids
 
     return labels, centroids
 
-
-# Generate example data
-np.random.seed(0)
-X = np.vstack([np.random.normal(loc=i, scale=1.0, size=(100, 2)) for i in range(1, 4)])
-
-# Define initial centroids
-centroids = np.array([[1, 1], [2, 2], [3, 3]])
+# Test data
+data = [(1, 1), (1.5, 2), (3, 3), (5, 7), (3.5, 5), (4.5, 5), (3.5, 4.5)]
 
 # Apply the k-means algorithm
-labels, final_centroids = k_means(X, centroids)
+labels, centroids = k_means(data, 2)
 
-# Display the results
-plt.scatter(X[:, 0], X[:, 1], c=labels)
-plt.scatter(final_centroids[:, 0], final_centroids[:, 1], c='red', marker='x')
-plt.title('K-Means Clustering Results')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.show()
+print("Labels:", labels)
+print("Centroids:", centroids)
+
